1faf83b750f0228a37d69e0b2e89ea9ef60b21eb
Graciela Romano <graci.romano@gmail.com>
2024-08-08 18:48:11 -0300

    Se agrego la vista chirps y se agrego a la navegacion

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,20 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::view('/','welcome')->name('welcome');
 
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard','dashboard')->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/chirps', function () {
+        return view('chirps.index');
+    })->name('chirps.index');
 });
 
 require __DIR__.'/auth.php';
