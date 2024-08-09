d7dfd29fbe3a58142b8b033fb2f4b873526ce086
Graciela Romano <graci.romano@gmail.com>
2024-08-09 20:51:35 -0300

    Modificacion de la vista, modelo y controlador de chirp

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -14,12 +15,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
-    Route::get('/chirps', function () {
-        return view('chirps.index');
-    })->name('chirps.index');
-    Route::post('/chirps', function () {
-        $message = request('message');
-    });
+    Route::get('/chirps', [ChirpController::class, 'index'])->name('chirps.index'); 
+        
+    
+    Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.store');
 });
 
 require __DIR__.'/auth.php';
